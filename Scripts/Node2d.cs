84cd6f5430713cc18613d55e98583b466f24e180
RobertAj0857 <robertaj0857@gmail.com>
2024-11-14 14:57:49 +0100

    Game Update
    Wizard money gang

--- Scripts/Node2d.cs
+++ Scripts/Node2d.cs
@@ -6,10 +6,16 @@ public partial class Node2d : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		int val = 10;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		for (int i = 0; i < 10; i++)
+		{
+			val += i;
+			GD.Print(val);
+		}
 	}
 }
