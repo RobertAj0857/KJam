3c477d08261a73b6c584a93c83bf17dabac5bbee
Duck-Joke <duckofjokes@gmail.com>
2024-11-16 20:12:18 +0100

    fireball improvement

--- Scripts/FireBall.cs
+++ Scripts/FireBall.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class FireBall : Projectile
 {
-	[Export]
 	private Sprite2D sprite;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -11,13 +10,17 @@ public partial class FireBall : Projectile
 		sprite = GetNode<Sprite2D>("Sprite2D");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	private void Rotate() {
 		if(Direction != Vector2.Zero) {
 			float dir = Direction.Angle();
 			GlobalRotation = dir;
 			sprite.GlobalRotation = 0;
 		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Rotate();
 	}
 }
