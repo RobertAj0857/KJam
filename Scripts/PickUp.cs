b7500c1f6d7f67c1dec234266f897d7b000489fe
ItsOnlyASkole <alexanderkbak@gmail.com>
2024-11-17 04:16:38 +0100

    Pickup increase size

--- Scripts/PickUp.cs
+++ Scripts/PickUp.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.ObjectModel;
+using System.Drawing;
 
 public partial class PickUp : Area2D
 {
@@ -15,18 +16,34 @@ public partial class PickUp : Area2D
 	[Export]
 	public CollisionShape2D collision;
 	public Spawner spawner;
+	
+	private bool isSpawned = false;
+	
+	private double spawnTime = 0.5;
+	private double timePassed = 0;
 	private PirateCraftingController.Element element;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		sprite2D.Scale = Vector2.Zero;
 	}
-
-	public override void _PhysicsProcess(double delta)
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+		if(!isSpawned){
+			Visible = true;
+			isSpawned = true;
+		}
+		timePassed += delta;
+		if(timePassed < spawnTime){
+			sprite2D.Scale = Vector2.One * (float)(timePassed / spawnTime);
+		}else if(sprite2D.Scale != Vector2.One){
+			sprite2D.Scale = Vector2.One;
+		}
+    }
+    public override void _PhysicsProcess(double delta)
 	{
 		base._PhysicsProcess(delta);
-
-
 		// checks for players
 		Godot.Collections.Array<Area2D> hits = GetOverlappingAreas();
 		bool cantPickUp = false;
